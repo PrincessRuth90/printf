a354635b66a75242f7751a3a770ef4e1093a9a82
PrincessRuth90 <marcusruth86@gmail.com>
2022-07-11 14:41:05 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
@@ -19,6 +47,13 @@ int print_base16_upper_lower(va_list arg, char *representation);
 int loop_format(va_list arg, const char *string);
 int call_function_manager(char aux, va_list arg);
 int check_percent(int *flag, char aux); 
-int get_size(const char *format, int *i)
+int get_size(const char *format, int *i);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
 
 #endif /*MAIN_H */ 
