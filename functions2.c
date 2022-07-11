e5e2a533d842dce18029838ca403e80393692ce5
root <mykeloriyomi@gmail.com>
2022-07-11 16:51:14 -0700

    Corrections and updates.

--- functions2.c
+++ functions2.c
@@ -48,7 +48,7 @@ int print_pointer(va_list types, char buffer[],
 
 	ind++;
 
-	/return (write(1, &buffer[i], BUFF_SIZE - i - 1));/
+	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
 }
