e0b05f44da5d4626a1139f89816be7b3cf4d94b0
PrincessRuth90 <marcusruth86@gmail.com>
2022-07-11 05:10:35 -0700

    main.h

--- main.h
+++ main.h
@@ -1,35 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
+void _puts(char *str);
+int _strlen(char *s);
 int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
-int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR (va_list arg);
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux); 
 
-/**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
- */
-typedef struct identifierStruct
-{
-char *indentifier;
-int (*printer)(va_list);
-} identifierStruct;
-
-#endif
+#endif /*MAIN_H */ 
