931a45ae03d193b009d2ae105e8e378adc4ecf72
root <mykeloriyomi@gmail.com>
2022-07-11 17:08:04 -0700

    Corrections and updates.

--- main.h
+++ main.h
@@ -85,11 +85,11 @@ int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
-/Function to print string in reverse/
+/*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/Function to print a string in rot 13/
+/*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
